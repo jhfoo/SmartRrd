ab0644060e967cab31b59b099b65fc5b64553b5b
jhfoo <foojihaw@hotmail.com>
2017-07-10 13:27:31 -0500

    Updated test route

--- src/server.js
+++ src/server.js
@@ -1,25 +1,40 @@
 const MovingAvg = require('./MovingAvg');
 
-var samples = [],
-    SampleCount = 10000;
+console.log('To test the algorithm try:\n'
+    + ' npm test\n'
+    + ' or\n'
+    + ' node src/server.js test <sample size>\n');
 
-// generate random samples
-for (var i = 0; i < SampleCount; i++) {
-    samples.push(Math.ceil(Math.random() * 100));
-}
+if (process.argv[2].toLowerCase() === 'test')
+    runSmaTest(process.argv[3]);
 
-var LastAvg = 0;
-for (var i = 0; i < SampleCount; i++) {
-    LastAvg = (LastAvg * i + samples[i]) / (i + 1);
-//    console.log(LastAvg);
-}
-var FinalHPAvg = LastAvg;
 
-console.log('SMA algorithm');
-LastAvg = 0;
-for (var i = 0; i < SampleCount; i++) {
-    LastAvg = MovingAvg.calculateAverage(LastAvg, i + 1, samples[i]);
-//    console.log(LastAvg);
+
+
+
+function runSmaTest(SampleCount) {
+    var samples = [],
+        SampleCount = SampleCount || 100000;
+
+    // generate random samples
+    console.log('Generating ' + SampleCount + ' samples');
+    for (var i = 0; i < SampleCount; i++) {
+        samples.push(Math.ceil(Math.random() * 100));
+    }
+
+    var LastAvg = 0;
+    for (var i = 0; i < SampleCount; i++) {
+        LastAvg = (LastAvg * i + samples[i]) / (i + 1);
+        //    console.log(LastAvg);
+    }
+    var FinalHPAvg = LastAvg;
+
+    console.log('SMA algorithm');
+    LastAvg = 0;
+    for (var i = 0; i < SampleCount; i++) {
+        LastAvg = MovingAvg.calculateAverage(LastAvg, i + 1, samples[i]);
+        //    console.log(LastAvg);
+    }
+    console.log('SMA calculation: ' + LastAvg);
+    console.log('High-precision average: ' + FinalHPAvg);
 }
-console.log('SMA calculation: ' + LastAvg);
-console.log('High-precision average: ' + FinalHPAvg);
